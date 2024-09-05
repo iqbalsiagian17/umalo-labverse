d2adcfd35d10ea79240aaa16342156a911929dd9
christian050104 <christianpanjaitan789@gmail.com>
2024-09-05 14:13:17 +0700

    alert
    alert

--- source_code/resources/views/Admin/Transaksi/index.blade.php
+++ source_code/resources/views/Admin/Transaksi/index.blade.php
@@ -12,55 +12,81 @@
             <div class="alert alert-success">
                 {{ session('success') }}
             </div>
-        @endif
+            @endif
 
-        <div class="card-body">
-            <div class="row">
-                <table class="table table-striped table-responsive table-hover">
-                    <thead class="thead-dark">
-                        <tr>
-                            <th>No</th>
-                            <th>ID Transaksi</th>
-                            <th>User</th>
-                            <th>Harga Total</th>
-                            <th>Status</th>
-                            <th>Aksi</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($orders as $index => $order)
-                        <tr>
-                            <td>{{ $index + 1 }}</td>
-                            <td>{{ $order->id }}</td>
-                            <td>{{ $order->user->name }}</td> 
-                            <td>
-                                @if($order->harga_setelah_nego)
-                                    <span style="text-decoration: line-through;">Rp {{ number_format($order->harga_total, 0, ',', '.') }}</span><br>
-                                    <span>Rp {{ number_format($order->harga_setelah_nego, 0, ',', '.') }}</span>
-                                @else
-                                    Rp {{ number_format($order->harga_total, 0, ',', '.') }}
-                                @endif
-                            </td>
-                            <td>{{ $order->status }}</td>
-                            <td>
-                                <a href="{{ route('transaksi.show', $order->id) }}" class="btn btn-info">Lihat</a>
-                                <a href="{{ route('transaksi.edit', $order->id) }}" class="btn btn-warning">Edit</a>
-                                <form action="{{ route('transaksi.destroy', $order->id) }}" method="POST" style="display:inline-block;">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button type="submit" class="btn btn-danger">Hapus</button>
-                                </form>
-                            </td>
-                        </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+            <div class="card-body">
+                <div class="row">
+                    <table class="table table-striped table-responsive table-hover">
+                        <thead class="thead-dark">
+                            <tr>
+                                <th>No</th>
+                                <th>ID Transaksi</th>
+                                <th>User</th>
+                                <th>Harga Total</th>
+                                <th>Status</th>
+                                <th>Aksi</th>
+                            </tr>
+                        </thead>
+                        <tbody id="produk-table-body">
+                            @foreach($orders as $index => $order)
+                            <tr>
+                                <td>{{ $index + 1 }}</td>
+                                <td>{{ $order->id }}</td>
+                                <td>{{ $order->user->name }}</td> 
+                                <td>
+                                    @if($order->harga_setelah_nego)
+                                        <span style="text-decoration: line-through;">Rp {{ number_format($order->harga_total, 0, ',', '.') }}</span><br>
+                                        <span>Rp {{ number_format($order->harga_setelah_nego, 0, ',', '.') }}</span>
+                                    @else
+                                        Rp {{ number_format($order->harga_total, 0, ',', '.') }}
+                                    @endif
+                                </td>
+                                <td>{{ $order->status }}</td>
+                                <td>
+                                    <a href="{{ route('transaksi.show', $order->id) }}" class="btn btn-info">Lihat</a>
+                                    <a href="{{ route('transaksi.edit', $order->id) }}" class="btn btn-warning">Edit</a>
+                                    <form action="{{ route('transaksi.destroy', $order->id) }}" method="POST" style="display:inline-block;">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="button" class="btn btn-danger delete-button">Hapus</button>
+                                    </form>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
             </div>
-        </div>
 
         </div>
     </div>
 </div>
 
-    
+<!-- Include SweetAlert2 JS -->
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.all.min.js"></script>
+
+<script>
+    $(document).ready(function() {
+        // SweetAlert confirmation for deletion
+        $('.delete-button').on('click', function(e) {
+            e.preventDefault(); // Prevent form submission
+            
+            var form = $(this).closest('form'); // Get the form
+            Swal.fire({
+                title: 'Are you sure?',
+                text: "You won't be able to revert this!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonText: 'Yes, delete it!',
+                cancelButtonText: 'No, cancel!',
+                reverseButtons: true
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    form.submit(); // Submit the form if confirmed
+                }
+            });
+        });
+    });
+</script>
+
 @endsection
